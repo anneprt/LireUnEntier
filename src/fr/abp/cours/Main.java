9e8ca5746759594a5d22109599733539ad5f6db2
Anne <patesdegeek@gmail.com>
2020-04-15 20:35:19 +0200

    lecture clavier +stockage+boucle

--- src/fr/abp/cours/Main.java
+++ src/fr/abp/cours/Main.java
@@ -1,8 +1,21 @@
 package fr.abp.cours;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+        String tmp = "";
+        char C = '\0';
+
+        System.out.println("Entrez des chiffres et appuyez sur la touche entrée pour valider la saisie: ");
+
+        while (C != '\n') {
+            C = (char) System.in.read();
+            if (C != '\r' && C != '\n') {
+                tmp = tmp + C;
+
+            }
+        }
     }
-}
+
